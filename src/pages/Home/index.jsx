50be0059c46c461ab9f189bd7147e3c9daf6a981
savifon <savifon1@gmail.com>
2022-02-10 20:11:43 -0300

    setando preço total do carrinho

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,7 @@ import data from "../../server/pizzas.json";
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const [priceCart, setPriceCart] = useState(0);
 
   useEffect(() => {
     const products = data.map((product, index) => ({
@@ -14,23 +15,15 @@ const Home = () => {
     }));
 
     setProducts(products);
-
-    // (async () => {
-    //   await api
-    //     .get("/pizzas")
-    //     .then((response) => {
-    //       const data = response.data.map((product, index) => ({
-    //         ...product,
-    //         id: index + 1,
-    //       }));
-
-    //       setProducts(data);
-    //     })
-    //     .catch((err) => {
-    //       console.error(err);
-    //     });
-    // })();
   }, []);
+
+  useEffect(() => {
+    setPriceCart(
+      cart.reduce((total, product) => {
+        return total + product.price * product.qty;
+      }, 0)
+    );
+  }, [cart]);
 
   const handleAddCart = (product) => {
     const found = cart.find((item) => item.id === product.id);
@@ -60,6 +53,23 @@ const Home = () => {
     }
   };
 
+  const handleCheckout = () => {
+    // (async () => {
+    //   await api
+    //     .get("/pizzas")
+    //     .then((response) => {
+    //       const data = response.data.map((product, index) => ({
+    //         ...product,
+    //         id: index + 1,
+    //       }));
+    //       setProducts(data);
+    //     })
+    //     .catch((err) => {
+    //       console.error(err);
+    //     });
+    // })();
+  };
+
   return (
     <>
       <h1>Home</h1>
@@ -81,7 +91,13 @@ const Home = () => {
       )}
 
       {cart.length ? (
-        cart.map((item) => <p>{JSON.stringify(item)}</p>)
+        <>
+          {cart.map((item) => (
+            <p>{JSON.stringify(item)}</p>
+          ))}
+          <p>{priceCart}</p>
+          <button onClick={() => handleCheckout()}>Confirmar pedido</button>
+        </>
       ) : (
         <p>Sem dados</p>
       )}
